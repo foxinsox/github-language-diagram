a8beb961181b2a4c1b8ea1dfddf9d1abbaaf4ef3
foxinsox <=>
2020-08-26 15:33:19 +0200

    added first tests

---
+++ components/UserSearch/UserCard/UserCard.test.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import UserCard from './UserCard';
+
+describe('UserCard', () => {
+    let expectedProps;
+
+    beforeEach(() => {
+        expectedProps = {
+            avatarUrl: 'https://avatars0.githubusercontent.com/u/8191065?v=4',
+            login: 'foxinsox',
+            type: 'User'
+        };
+    });
+
+    test('should do something',() => {
+        const { getByText, getByAltText } = render(<UserCard {...expectedProps} />);
+        const login = getByText(expectedProps.login);
+        const type = getByText(expectedProps.type);
+        const image = getByAltText(expectedProps.login);
+
+        expect(login).toBeVisible;
+        expect(type).toBeVisible;
+        expect(image).toBeVisible;
+    });
+});
